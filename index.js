fc239757ab9610d0e4d94be298ff9ca99c6c7f0d
Ray Suelzer <rsuelzer@gmail.com>
2021-08-30 14:35:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('graphql-import-node');
+const typeDefs = require('./ab_schema.graphql');
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+
+const { generateAll, generateQuery } =  require('gql-generator-node');
+
+const schema = makeExecutableSchema({ typeDefs });
+
+// const {queries, mutations} = generateAll(schema, 4, ({args})=>{
+//   const o = {};
+//   (args || []).forEach(arg=>{
+//       o[arg.name] = arg;
+//   });
+//   return o;
+// });
+
+
+const query = generateQuery({
+  field: schema
+      .getQueryType()
+      .getFields().getEntity,
+  depthLimit: 8,
+  dedupe: true
+})
+
+require("fs").writeFile("getEntity.gql", query , (err) => {})
